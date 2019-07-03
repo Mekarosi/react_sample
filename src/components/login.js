fb6dd02f9d7b82b4291d444eed007d23cc957271
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-07-03 16:26:40 +0100

    Added form fields using Bootstrap to login form

--- src/components/login.js
+++ src/components/login.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export default class login extends Component {
     constructor(props){
-        super(this.props)
+        super(props)
         this.state = {
-            
+            email: "",
+            password: ""
+
         }
     }
 
     render() {
         return (
             <div>
-                
+                <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
             </div>
         )
     }
