98758cbde95839ee2961f15585826aa45b11c800
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-07-03 16:01:28 +0100

    Added login component

---
+++ src/components/login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class login extends Component {
+    constructor(props){
+        super(this.props)
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
